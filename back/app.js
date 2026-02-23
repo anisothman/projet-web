a9121a8a93d19c4888192adf7919c9ebcedaa91b
Amen Allah Sahli <amensahli381@gmail.com>
2026-02-23 23:54:41 +0100

    initialisation supabase

---
+++ back/app.js
@@ -0,0 +1,6 @@
+const supabaseUrl = 'https://zfnhtobgkkdmscnscpwh.supabase.co';
+const supabaseKey = 'sb_publishable_3ogDZw3U19agKboWnQCSeg_B9WV9Xn_';
+const client = supabase.createClient(supabaseUrl, supabaseKey);
+
+// Database "Client" contains id,name,email
+
